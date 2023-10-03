a72a6de513fb829b05215f352393a201fe859074
Vinay <97431634+VinayGeek@users.noreply.github.com>
2023-10-03 18:21:57 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SiderBar from "./components/sideNavbar";
 import AppStyle from "./app.module.css";
 import Dashboard from "./pages/Dashboard";
 import RightBar from "./pages/Rightbar";
+import SignIn from "./pages/Login/index";
 
 function App() {
   return (
-    <div className={AppStyle.container}>
-      <SiderBar />
-      <div className={AppStyle["col-span-2"]}>
-        <Dashboard />
-      </div>
-      <RightBar />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<SignIn />} />
+        <Route
+          exact
+          path="/dashboard"
+          element={
+            <div className={AppStyle.container}>
+              <SiderBar />
+              <div className={AppStyle["col-span-2"]}>
+                <Dashboard />
+              </div>
+              <RightBar />
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
